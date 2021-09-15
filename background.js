35c049f3cdca66ac0f9e2ff04b411334d18d85d5
BenjaminRochez <rochez.benjamin@gmail.com>
2021-09-15 11:46:05 +0200

    Added injection effect

--- background.js
+++ background.js
@@ -5,4 +5,21 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-chrome.storage.local.get("name", (data) => {});
+//chrome.storage.local.get("name", (data) => {});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(tabId);
+  console.log(changeInfo);
+  console.log(tab);
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        files: ["./foreground.js"],
+      })
+      .then(() => {
+        console.log("Injected Script");
+      })
+      .catch((err) => console.log(err));
+  }
+});
---
+++ foreground.js
@@ -0,0 +1,6 @@
+document.querySelector(
+  "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+).style.transitionDuration = "2s";
+document.querySelector(
+  "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+).style.transform = "rotate(180deg)";
